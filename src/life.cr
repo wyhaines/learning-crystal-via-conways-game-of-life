b71eba0f3a93910997c89e95e3adae884503894e
Kirk Haines <wyhaines@gmail.com>
2023-05-18 12:41:10 -0600

    Add the step-3 reference implemementation.

--- src/life.cr
+++ src/life.cr
@@ -1,5 +1,19 @@
 class Life
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  @board : Array(Array(Bool))
+
+  def initialize(@width : Int32, @height : Int32)
+    @board = Array.new(@height) { Array.new(@width, false) }
+    setup
+  end
+
+  def set_alive(x, y)
+    @board[y][x] = true
+  end
+
+  def setup
+    ((@height * @width) // 4).times { set_alive rand(@width), rand(@height) }
+  end
+
 end
