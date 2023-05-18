b037f527dfc7acfa631c81bf77b9d9f835706896
Kirk Haines <wyhaines@gmail.com>
2023-05-18 12:41:47 -0600

    Add the step-4 reference implemementation.

--- src/life.cr
+++ src/life.cr
@@ -1,5 +1,33 @@
 class Life
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  @board : Array(Array(Bool))
+
+  def initialize(@width : Int32, @height : Int32)
+    @board = Array.new(@height) { Array.new(@width, false) }
+    setup
+  end
+
+  def set_alive(x, y)
+    @board[y][x] = true
+  end
+
+  def setup
+    ((@height * @width) // 4).times { set_alive rand(@width), rand(@height) }
+  end
+
+  def print_board
+    print "\e[2J\e[f" # Clear screen
+    @board.each do |row|
+      row.each do |cell|
+        print cell ? 'O' : '.'
+      end
+      puts
+    end
+  end
+
+  def run
+    print_board
+  end
+
 end
